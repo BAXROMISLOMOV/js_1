26c64260e746dad25a9362246852e4778f125d8a
Baxrom <bahromislomov324@gmail.com>
2024-10-14 22:58:15 +0500

    homework comlpeated

---
+++ main.js
@@ -0,0 +1,88 @@
+console.log(`
+   Ustoz:Baxtiyor aka
+   1:Baxrom
+   2:Ozod
+   3:Abdulloh
+   4:Jaxongir
+   5:Dilshod
+   6:Nazirov Ozod
+   7:Samandar
+   8:Abdulaziz
+   9:Asad
+   10:Zarifa
+   11:Muhammad
+   12:Oybek
+   `);
+let ism = prompt("Ismingizni kiriting!");
+let familya = prompt("Familyangizni kiriting!");
+let yoshi = prompt("Yoshingizni kiriting!");
+let yashashJoyi = prompt("Manzilingizni kiriting!");
+let telefonNomer = prompt("Telefon nomerni kiriting!");
+
+console.log(`
+     Sizning ismigiz: ${ism},
+     Sizning familyangiz: ${familya},
+     Sizning yoshingiz: ${yoshi},
+     Sizning yashash joyingiz :${yashashJoyi},
+     Sizning telefon raqamingiz: ${telefonNomer},
+    `);
+
+alert(`
+    Sizning ismigiz: ${ism},
+    Sizning familyangiz: ${familya},
+    Sizning yoshingiz: ${yoshi},
+    Sizning yashash joyingiz :${yashashJoyi},
+    Sizning telefon raqamingiz: ${telefonNomer},
+   `);
+
+
+let product = prompt("Product name")
+let pRoduct = prompt("Product name")
+let prOduct = prompt("Product name")
+let proDuct = prompt("Product name")
+let prodUct = prompt("Product name")
+
+console.log(
+    `
+     UMUMIY : ${product},
+            : ${pRoduct},
+            : ${prOduct},
+            :${proDuct},
+            : ${prodUct},
+   `
+)
+alert(
+    `
+     UMUMIY : ${product},
+            : ${pRoduct},
+            : ${prOduct},
+            :${proDuct},
+            : ${prodUct},
+   `
+)
+
+
+
+
+
+
+   let Pe = +prompt("Matematikadan olgan balingizini kiriting");
+let english = +prompt("Ingliz tilidan olgan balingizini kiriting");
+let rus = +prompt("Rus tilidan olgan balingizini kiriting");
+let biology = +prompt("Tarixan olgan balingizini kiriting");
+let fizika = +prompt("Fizikidan olgan balingizini kiriting");
+
+alert(
+  `Sizning umumiy o'rtacha balingiz: ${
+    (Pe + english + rus + biology + fizika) / 5
+  }`
+  
+);
+
+console.log(
+  `Sizning umumiy o'rtacha balingiz: ${
+    (Pe + english + rus + biology + fizika) / 5
+  }`
+);
+
+
